eb94d057e062d8da0da369c09c20a1678b809256
Michael Spector <michaelspector@gmail.com>
2017-03-02 13:21:14 -0500

    renamed package

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='OpenReviewMatching',
+setup(name='openreview-matching',
       version='0.1',
       description='OpenReview matching library',
       url='https://github.com/iesl/openreview-matching',
