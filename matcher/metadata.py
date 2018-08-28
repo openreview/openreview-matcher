bf3143db80b698664b11f9f3645ee07059c7619f
Michael Spector <michaelspector@gmail.com>
2018-08-28 12:42:58 -0400

    reorg

--- matcher/metadata.py
+++ matcher/metadata.py
@@ -6,120 +6,13 @@ import numpy as np
 
 from . import utils
 
-class OpenReviewFeature(object):
-  """
-  Defines an abstract base class for OpenReview matching features.
-
-  Classes that extend this object must implement a method called "score" with the following arguments: (signature, forum)
-
-  Example:
-
-  def score(signature, forum):
-    ## compute feature score
-    return score
-
-  """
-  __metaclass__ = abc.ABCMeta
-
-  @abc.abstractmethod
-  def score(self, signature, forum):
-    """
-    @signature - tilde ID of user
-    @forum - forum of paper
-
-    """
-    return 0.0
-
-class BasicAffinity(OpenReviewFeature):
-  """
-  This is an OpenReviewFeature that uses the experimental "expertise ranks" endpoint.
-  """
-
-  def __init__(self, name, client, groups, papers):
-    """
-    client - the openreview.Client object used to make the network calls
-    groups - an array of openreview.Group objects representing the groups to be matched
-    papers - an array of openreview.Note objects representing the papers to be matched
-    """
-
-    self.name = name
-    self.client = client
-    self.groups = groups
-    self.papers = papers
-
-    self.scores_by_user_by_forum = {n.forum: defaultdict(lambda:0) for n in papers}
-
-    for g in groups:
-      for n in papers:
-        response = requests.get(
-          self.client.baseurl+'/reviewers/scores?group={0}&forum={1}'.format(g.id, n.forum),
-          headers=self.client.headers)
-        self.scores_by_user_by_forum[n.forum].update({r['user']: r['score'] for r in response.json()['scores']})
-
-  def score(self, signature, forum):
-    return self.scores_by_user_by_forum[forum][signature]
-
-
-def generate_metadata_notes(client, papers, metadata_invitation, match_group, score_maps={}, constraint_maps={}):
-  """
-  Generates a list of metadata notes
-
-  Returns:
-    a list of openreview.Note objects, each representing a metadata.
-
-  '''
-  Score and constraint maps should be two-dimensional dicts,
-  where the first index is the forum ID, and the second index is the user ID.
-  '''
-
-  """
-
-  # unpack variables
-  papers_by_forum = {n.forum: n for n in papers}
-
-  # make network calls
-  print("getting metadata...")
-  metadata_notes = [n for n in openreview.utils.get_all_notes(client, metadata_invitation.id) if n.forum in papers_by_forum]
-  print("done")
-  existing_metadata_by_forum = {m.forum: m for m in metadata_notes}
-
-  default_params = {
-    'invitation': metadata_invitation.id,
-    'readers': metadata_invitation.reply['readers']['values'],
-    'writers': metadata_invitation.reply['writers']['values'],
-    'signatures': metadata_invitation.reply['signatures']['values'],
-    'content': {'groups':{}}
-  }
-
-  new_metadata = []
-
-  for p in papers:
-    if p.forum not in existing_metadata_by_forum:
-      metadata_params = dict(default_params, **{'forum': p.forum})
-    else:
-      metadata_params = existing_metadata_by_forum[p.forum].to_json()
-
-    metadata_params['content'] = {'groups': {match_group.id: []}}
-    new_entries = []
-
-    for user_id in match_group.members:
-      metadata_params['content']['groups'][match_group.id].append({
-        'userId': user_id,
-        'scores': {name: score_map.get(p.forum, {}).get(user_id, 0) for name, score_map in score_maps.items() if score_map.get(p.forum, {}).get(user_id, 0) > 0},
-        'constraints': {name: constraint_map.get(p.forum, {}).get(user_id) for name, constraint_map in constraint_maps.items() if constraint_map.get(p.forum, {}).get(user_id)}
-      })
-
-    updated_metadata_note = openreview.Note(**metadata_params)
-    new_metadata.append(updated_metadata_note)
-
-  return new_metadata
-
 class Encoder(object):
-  def __init__(self, metadata=None, config=None, reviewer_ids=None):
+  def __init__(self, metadata=None, config=None, reviewer_ids=None, cost_func=utils.cost):
 
     self.metadata = []
     self.config = {}
     self.reviewer_ids = []
+    self.cost = cost_func
 
     self.cost_matrix = np.zeros((0,0))
     self.constraint_matrix = np.zeros((0,0))
@@ -133,9 +26,9 @@ class Encoder(object):
     self.constraints = config['constraints']
 
     if metadata and config and reviewer_ids:
-      self.encode(metadata, config, reviewer_ids)
+      self.encode(metadata, config, reviewer_ids, cost_func)
 
-  def encode(self, metadata, config, reviewer_ids):
+  def encode(self, metadata, config, reviewer_ids, cost_func):
     '''
     Encodes the cost and constraint matrices to be used by the solver.
 
@@ -148,6 +41,7 @@ class Encoder(object):
     self.metadata = metadata
     self.config = config
     self.reviewer_ids = reviewer_ids
+    self.cost_func = cost_func
 
     self.cost_matrix = np.zeros((len(self.reviewer_ids), len(self.metadata)))
     self.constraint_matrix = np.zeros(np.shape(self.cost_matrix))
@@ -179,7 +73,7 @@ class Encoder(object):
         coordinates = reviewer_index, paper_index
         entry = entry_by_id.get(id)
         if entry:
-          self.cost_matrix[coordinates] = cost(entry['scores'], self.weights)
+          self.cost_matrix[coordinates] = self.cost_func(entry['scores'], self.weights)
 
           if entry.get('conflicts'):
             self.constraint_matrix[coordinates] = -1
@@ -232,7 +126,5 @@ class Encoder(object):
     return assignments_by_forum, alternates_by_forum
 
 
-def cost(scores, weights, precision=0.01):
-  weighted_scores = utils.weight_scores(scores, weights)
-  score_sum = sum(weighted_scores.values())
-  return -1 * int(score_sum / precision)
+
+
--- matcher/utils.py
+++ matcher/utils.py
@@ -4,7 +4,18 @@ import openreview
 
 def weight_scores(scores, weights):
     '''
-    multiplies feature values by weights, excepting hard constraints
+    scores: a dict of scores keyed on score name
+
+    weights: a dict of weights keyed on score name
+
+    returns a dict of weighted scores, keyed on score names found in @weights
+    (i.e. result ignores values in @scores that are not present in @weights)
+
+    Example:
+
+    >>> weight_scores({'tpms': 0.5, 'bid': 1.0 }, { 'tpms': 1.5 })
+    {'tpms': 0.75}
+
     '''
     weighted_scores = {}
     for feature in weights:
@@ -13,81 +24,70 @@ def weight_scores(scores, weights):
 
     return weighted_scores
 
+def cost(scores, weights, precision=0.01):
+  weighted_scores = utils.weight_scores(scores, weights)
+  score_sum = sum(weighted_scores.values())
+  return -1 * int(score_sum / precision)
+
 def get_conflicts(author_profiles, user_profile):
 
-    author_domains = set()
-    author_emails = set()
-    author_relations = set()
+  author_domains = set()
+  author_emails = set()
+  author_relations = set()
 
-    for author_email, profile in author_profiles.items():
+  for author_email, profile in author_profiles.items():
 
-        author_info = get_author_info(profile, author_email)
+    author_info = get_author_info(profile, author_email)
 
-        author_domains.update(author_info['domains'])
-        author_emails.update(author_info['emails'])
-        author_relations.update(author_info['relations'])
+    author_domains.update(author_info['domains'])
+    author_emails.update(author_info['emails'])
+    author_relations.update(author_info['relations'])
 
-    user_info = get_profile_info(user_profile)
+  user_info = get_profile_info(user_profile)
 
-    conflicts = set()
-    conflicts.update(author_domains.intersection(user_info['domains']))
-    conflicts.update(author_relations.intersection(user_info['emails']))
-    conflicts.update(author_emails.intersection(user_info['relations']))
+  conflicts = set()
+  conflicts.update(author_domains.intersection(user_info['domains']))
+  conflicts.update(author_relations.intersection(user_info['emails']))
+  conflicts.update(author_emails.intersection(user_info['relations']))
 
-    return list(conflicts)
+  return list(conflicts)
 
 def get_author_info(profile, email):
-    if profile:
-        return get_profile_info(profile)
-    else:
-        return {
-            'domains': get_domains(email, subdomains = True),
-            'emails': [email],
-            'relations': []
-        }
-
+  if profile:
+    return get_profile_info(profile)
+  else:
+    return {
+      'domains': openreview.tools.subdomains(email, subdomains = True),
+      'emails': [email],
+      'relations': []
+    }
 
 def get_profile_info(profile):
 
-    domains = set()
-    emails = set()
-    relations = set()
+  domains = set()
+  emails = set()
+  relations = set()
 
-    ## Emails section
-    for e in profile.content['emails']:
-        domains.update(get_domains(e, subdomains = True))
-        emails.add(e)
+  ## Emails section
+  for e in profile.content['emails']:
+    domains.update(openreview.tools.subdomains(e, subdomains = True))
+    emails.add(e)
 
-    ## Institution section
-    for h in profile.content.get('history', []):
-        domain = h.get('institution', {}).get('domain', '')
-        domains.update(get_domains(domain, subdomains = True))
+  ## Institution section
+  for h in profile.content.get('history', []):
+    domain = h.get('institution', {}).get('domain', '')
+    domains.update(openreview.tools.subdomains(domain, subdomains = True))
 
 
-    ## Relations section
-    relations.update([r['email'] for r in profile.content.get('relations', [])])
+  ## Relations section
+  relations.update([r['email'] for r in profile.content.get('relations', [])])
 
-    ## Filter common domains
-    if 'gmail.com' in domains:
-        domains.remove('gmail.com')
+  ## Filter common domains
+  if 'gmail.com' in domains:
+    domains.remove('gmail.com')
 
-    return {
-        'domains': domains,
-        'emails': emails,
-        'relations': relations
-    }
-
-def get_domains(entity, subdomains = False):
-
-    if '@' in entity:
-        full_domain = entity.split('@')[1]
-    else:
-        full_domain = entity
-
-    if subdomains:
-        domain_components = full_domain.split('.')
-        domains = ['.'.join(domain_components[index:len(domain_components)]) for index, path in enumerate(domain_components)]
-        valid_domains = [d for d in domains if '.' in d]
-        return valid_domains
-    else:
-        return full_domain
+  return {
+    'domains': domains,
+    'emails': emails,
+    'relations': relations
+  }
