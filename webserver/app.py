3a48bae99e05573808ad35f58e476e00c7ada9a0
Michael Spector <michaelspector@gmail.com>
2018-08-29 14:27:00 -0400

    clear assignments when recomputing

--- webserver/app.py
+++ webserver/app.py
@@ -10,11 +10,8 @@ import matcher
 
 app = Flask(__name__)
 
-# Does the task (and takes the arg which is how long this task will run before complete)
-
 def run_match(config_note, client):
-
-
+    start_time = time.time()
     config = config_note.content
     metadata = list(openreview.tools.iterget_notes(client, invitation=config['metadata_invitation']))
     reviewer_group = client.get_group(config['match_group'])
@@ -27,27 +24,33 @@ def run_match(config_note, client):
     supplies = [config['max_papers']] * len(reviewer_ids)
     demands = [config['max_users']] * len(metadata)
 
-    # enter "processing" phase
+    # enter 'processing' phase
     config_note.content['status'] = 'processing'
     config_note = client.post_note(config_note)
+
+    print('clearing existing assignments...')
+    clear_time = time.time()
+    for assignment_note in openreview.tools.iterget_notes(client, invitation=assignment_inv.id):
+        client.delete_note(assignment_note)
+    print('took {0:.2f} seconds'.format(time.time() - clear_time))
 
     # instantiate the metadata encoder, and use it to instantiate a flow solver
     print('instantiating encoder and solver...')
     instantiate_time = time.time()
     encoder = matcher.metadata.Encoder(metadata, config, reviewer_ids)
     flow_solver = matcher.Solver(supplies, demands, encoder.cost_matrix, encoder.constraint_matrix)
-    print("took {0:.2f} seconds".format(time.time() - instantiate_time))
+    print('took {0:.2f} seconds'.format(time.time() - instantiate_time))
 
     print('finding solution...')
     solution_time = time.time()
     solution = flow_solver.solve()
-    print("took {0:.2f} seconds".format(time.time() - solution_time))
+    print('took {0:.2f} seconds'.format(time.time() - solution_time))
 
     # decode the solution matrix
     print('decoding solution...')
     decoding_time = time.time()
     assignments_by_forum, alternates_by_forum = encoder.decode(solution)
-    print("took {0:.2f} seconds".format(time.time() - decoding_time))
+    print('took {0:.2f} seconds'.format(time.time() - decoding_time))
 
     print('posting assignments...')
     posting_time = time.time()
@@ -65,13 +68,13 @@ def run_match(config_note, client):
                 'alternateGroups': []
             }
         }))
-    print("took {0:.2f} seconds".format(time.time() - posting_time))
+    print('took {0:.2f} seconds'.format(time.time() - posting_time))
 
     config_note.content['status'] = 'complete'
     client.post_note(config_note)
-    print('done')
+    print('done. total time: {0:.2f} seconds'.format(time.time() - start_time))
 
-@app.route("/match", methods=['POST', 'OPTIONS'])
+@app.route('/match', methods=['POST', 'OPTIONS'])
 def match():
     print('matching')
 
@@ -83,9 +86,6 @@ def match():
 
     args = (config_note, client)
 
-    # print('config pending: {}'.format(config_note.id))
-
-    # starts the task myTask
     match_thread = Thread(
         target=run_match,
         args=args
@@ -94,5 +94,5 @@ def match():
 
     return 'matching done'
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
