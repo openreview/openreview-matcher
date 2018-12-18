6661f0826dd240952ffc64c3e8e8bfe2476d5082
marshall62 <marshall62@gmail.com>
2018-12-18 15:17:00 -0500

    fixed gunicorn startup command so that app starts correctly with logging

--- startsite.sh
+++ startsite.sh
@@ -1,2 +1,2 @@
 export FLASK_APP=matcher/app.py
-/home/openreview/openreview-matcher/venv/bin/gunicorn -b 10.128.0.23:5000 -w 10 matcher:app 
+/home/openreview/openreview-matcher/venv/bin/gunicorn -b 10.128.0.23:5000 -w 4 matcher.app:app
