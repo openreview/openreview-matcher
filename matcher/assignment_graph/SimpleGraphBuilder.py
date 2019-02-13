b86f54d375c112afb172c08791d7fde2613037ad
Michael Spector <michaelspector@gmail.com>
2019-02-13 09:13:16 -0500

    refactor for greater readability and code reuse

--- matcher/assignment_graph/SimpleGraphBuilder.py
+++ matcher/assignment_graph/SimpleGraphBuilder.py
@@ -23,17 +23,16 @@ class SimpleGraphBuilder(GraphBuilder):
                     r_node.index, p_node.index]
 
                 if arc_constraint in [0, 1]:
-                    graph.start_nodes.append(r_node)
-                    graph.end_nodes.append(p_node)
-                    graph.capacities.append(1)
 
                     # arc_constraint of 0 means there's no constraint;
                     # apply the cost as normal.
                     if arc_constraint == 0:
-                        graph.costs.append(int(arc_cost))
+                        cost = int(arc_cost)
 
                     # arc_constraint of 1 means that this user was explicitly
                     # assigned to this paper;
                     # set the cost as 1 less than the minimum cost.
                     if arc_constraint == 1:
-                        graph.costs.append(int(graph.min_cost - 1))
+                        cost = int(graph.min_cost - 1)
+
+                    graph.add_edge(r_node, p_node, 1, cost)
