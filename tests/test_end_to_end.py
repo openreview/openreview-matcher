7a2994bb0c96fdc3e07dae6bc270e51231b72001
marshall62 <marshall62@gmail.com>
2019-03-11 16:53:01 -0400

    Stopped using unittest and alterred test setup to use pytest with a test_util fixture coming from conftest.py

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -1,24 +1,20 @@
-import unittest
+
 import matcher
 import os
 
 from helpers.display_conf import DisplayConf
 from matcher.fields import Configuration
 from params import Params
-from helpers.test_util import TestUtil
 from helpers.assignment_checker import AssignmentChecker
 
 # Note Well:  To run this test you must be running OR with a clean db.  See README for details.
 
-class TestEndToEnd(unittest.TestCase):
+class TestEndToEnd():
 
     # called once at beginning of suite
+    # See conftest.py for other run-once setup that is part of the test_util fixture passed to each test.
     @classmethod
-    def setUpClass(cls):
-        # Flask provides a test client so that endpoints of the matcher app can be called directly and given inputs exactly as
-        # they would in the field.
-        cls.flask_app_test_client = matcher.app.test_client()
-        cls.flask_app_test_client.testing = True
+    def setup_class(cls):
 
         # Turn off logging in the web app so that tests run with a clean console
         matcher.app.logger.disabled = True
@@ -29,12 +25,8 @@ class TestEndToEnd(unittest.TestCase):
         # so this config option is set here
         matcher.app.config['OPENREVIEW_BASEURL'] = cls.or_baseurl
 
-        cls.silent = True # shut off all printing
-        cls.test_util = TestUtil.get_instance(TestEndToEnd.or_baseurl, TestEndToEnd.flask_app_test_client, silent=cls.silent)
-
-
     @classmethod
-    def tearDownClass(cls):
+    def teardown_class(cls):
         pass
 
     # called at the beginning of each test
@@ -46,8 +38,7 @@ class TestEndToEnd(unittest.TestCase):
         pass
 
 
-    # @unittest.skip
-    def test1_10papers_7reviewers (self):
+    def test1_10papers_7reviewers (self, test_util):
         '''
         Tests 10 papers each requiring 1 review.  7 users each capable of giving 2 reviews.
         Expects:  produce an assignment
@@ -57,16 +48,15 @@ class TestEndToEnd(unittest.TestCase):
                   Params.NUM_REVIEWS_NEEDED_PER_PAPER: 1,
                   Params.REVIEWER_MAX_PAPERS: 2,
             })
-        self.test_util.set_test_params(params)
-        self.test_util.test_matcher()
-        conference = self.test_util.get_conference()
+        test_util.set_test_params(params)
+        test_util.test_matcher()
+        conference = test_util.get_conference()
         assert conference.get_config_note_status() == Configuration.STATUS_COMPLETE, \
             "Failure: Config status is {} expected {}".format(conference.get_config_note_status(), Configuration.STATUS_COMPLETE)
         assert len(conference.get_assignment_notes()) == len(conference.get_paper_notes()), "Number of assignments {} is not same as number of papers {}". \
             format(len(conference.get_assignment_notes()), len(conference.get_paper_notes()))
 
-    # @unittest.skip
-    def test2_10papers_7reviewers_5cust_load_5shortfall (self):
+    def test2_10papers_7reviewers_5cust_load_5shortfall (self, test_util):
         '''
         Tests 10 papers each requiring 2 reviews.  7 users each capable of giving 3 reviews.  Custom_loads will reduce supply by 5
         Expects:  Failure because supply (16) < demand (20)
@@ -77,16 +67,15 @@ class TestEndToEnd(unittest.TestCase):
                   Params.REVIEWER_MAX_PAPERS: 3,
                   Params.CUSTOM_LOAD_CONFIG: {Params.CUSTOM_LOAD_SUPPLY_DEDUCTION: 5}
             })
-        self.test_util.set_test_params(params)
-        self.test_util.test_matcher()
-        conference = self.test_util.get_conference()
+        test_util.set_test_params(params)
+        test_util.test_matcher()
+        conference = test_util.get_conference()
         assert conference.get_config_note_status() == Configuration.STATUS_ERROR, \
             "Failure: Config status is {} expected {}".format(conference.get_config_note_status(), Configuration.STATUS_ERROR)
         assert len(conference.get_assignment_notes()) == 0, "Assignment notes should not be created if match fails"
 
 
-    # @unittest.skip
-    def test3_10papers_7reviewers_0cust_load (self):
+    def test3_10papers_7reviewers_0cust_load (self, test_util):
         '''
         Tests 10 papers each requiring 2 reviews.  7 users each capable of giving 3 reviews.  Custom_loads will reduce supply by 0
         Expects:  Successful production of assignment
@@ -97,9 +86,9 @@ class TestEndToEnd(unittest.TestCase):
                   Params.REVIEWER_MAX_PAPERS: 3,
                   Params.CUSTOM_LOAD_CONFIG: {Params.CUSTOM_LOAD_SUPPLY_DEDUCTION: 0}
             })
-        self.test_util.set_test_params(params)
-        self.test_util.test_matcher()
-        conference = self.test_util.get_conference()
+        test_util.set_test_params(params)
+        test_util.test_matcher()
+        conference = test_util.get_conference()
         assert conference.get_config_note_status() == Configuration.STATUS_COMPLETE, \
             "Failure: Config status is {} expected {}".format(conference.get_config_note_status(), Configuration.STATUS_COMPLETE)
         assert len(conference.get_assignment_notes()) == len(conference.get_paper_notes()), \
@@ -112,8 +101,7 @@ class TestEndToEnd(unittest.TestCase):
                 " custom_load " +custom_load+ " exceeded.  Papers assigned: " + review_count_map[reviewer]
 
 
-    # @unittest.skip
-    def test4_10papers_7reviewers_5cust_load_excess (self):
+    def test4_10papers_7reviewers_5cust_load_excess (self, test_util):
         '''
         Tests 10 papers each requiring 2 reviews.  7 users each capable of giving 4 reviews.  Custom_loads will reduce supply by 5
         Expects:  Successful production of assignment
@@ -124,9 +112,9 @@ class TestEndToEnd(unittest.TestCase):
                   Params.REVIEWER_MAX_PAPERS: 4,
                   Params.CUSTOM_LOAD_CONFIG: {Params.CUSTOM_LOAD_SUPPLY_DEDUCTION: 5}
             })
-        self.test_util.set_test_params(params)
-        self.test_util.test_matcher()
-        conference = self.test_util.get_conference()
+        test_util.set_test_params(params)
+        test_util.test_matcher()
+        conference = test_util.get_conference()
         assert conference.get_config_note_status() == Configuration.STATUS_COMPLETE, \
             "Failure: Config status is {} expected {}".format(conference.get_config_note_status(), Configuration.STATUS_COMPLETE)
         assert len(conference.get_assignment_notes()) == len(conference.get_paper_notes()), \
@@ -138,8 +126,8 @@ class TestEndToEnd(unittest.TestCase):
             assert review_count_map[reviewer] <= custom_load, "Reviewer " + reviewer + \
                 " custom_load " +custom_load+ " exceeded.  Papers assigned: " + review_count_map[reviewer]
 
-    # @unittest.skip
-    def test5_10papers_7reviewers_4locks (self):
+
+    def test5_10papers_7reviewers_4locks (self, test_util):
         '''
         Tests 10 papers each requiring 2 reviews.  7 users each capable of giving 4 reviews.  Constraints lock in users to 4 papers
         Expects:  Successful production of assignment where locked user are assigned to the papers they are locked to.
@@ -153,9 +141,9 @@ class TestEndToEnd(unittest.TestCase):
                       Params.CONSTRAINTS_LOCKS: {0: [4], 2: [4], 4: [1], 5: [1]}
                   }
             })
-        self.test_util.set_test_params(params)
-        self.test_util.test_matcher()
-        conference = self.test_util.get_conference()
+        test_util.set_test_params(params)
+        test_util.test_matcher()
+        conference = test_util.get_conference()
         assert conference.get_config_note_status() == Configuration.STATUS_COMPLETE, \
             "Failure: Config status is {} expected {}".format(conference.get_config_note_status(), Configuration.STATUS_COMPLETE)
         assert len(conference.get_assignment_notes()) == len(conference.get_paper_notes()), \
@@ -171,8 +159,7 @@ class TestEndToEnd(unittest.TestCase):
                         "Reviewer {} was locked to paper {} by constraint but not found in assignment".format(reviewer,forum_id)
 
 
-    # @unittest.skip
-    def test6_10papers_7reviewers_8vetos (self):
+    def test6_10papers_7reviewers_8vetos (self, test_util):
         '''
         Tests 10 papers each requiring 2 reviews.  7 users each capable of giving 4 reviews.  Constraints veto users in 4 papers
         Expects:  Successful production of assignment where vetoed users do are not assigned to papers they were vetoed from.
@@ -186,9 +173,9 @@ class TestEndToEnd(unittest.TestCase):
                       Params.CONSTRAINTS_VETOS : {0: [1,2], 1: [1,2], 2: [1,2,3], 3: [5]}
                   }
             })
-        self.test_util.set_test_params(params)
-        self.test_util.test_matcher()
-        conference = self.test_util.get_conference()
+        test_util.set_test_params(params)
+        test_util.test_matcher()
+        conference = test_util.get_conference()
         assert conference.get_config_note_status() == Configuration.STATUS_COMPLETE, \
             "Failure: Config status is {} expected {}".format(conference.get_config_note_status(), Configuration.STATUS_COMPLETE)
         assert len(conference.get_assignment_notes()) == len(conference.get_paper_notes()), \
@@ -207,8 +194,7 @@ class TestEndToEnd(unittest.TestCase):
             DisplayConf(conference, self.silent).show_assignment()
             raise exc
 
-    # @unittest.skip
-    def test7_10papers_7reviewers_3vetos (self):
+    def test7_10papers_7reviewers_3vetos (self, test_util):
         '''
         Tests 10 papers each requiring 2 reviews.  7 users each capable of giving 4 reviews.  Constraints veto users in 3 papers
         Expects:  Successful production of assignment where vetoed users do are not assigned to papers they were vetoed from.
@@ -222,9 +208,9 @@ class TestEndToEnd(unittest.TestCase):
                       Params.CONSTRAINTS_VETOS : {0: [0], 1: [1], 2: [2]}
                   }
             })
-        self.test_util.set_test_params(params)
-        self.test_util.test_matcher()
-        conference = self.test_util.get_conference()
+        test_util.set_test_params(params)
+        test_util.test_matcher()
+        conference = test_util.get_conference()
         assert conference.get_config_note_status() == Configuration.STATUS_COMPLETE, \
             "Failure: Config status is {} expected {}".format(conference.get_config_note_status(), Configuration.STATUS_COMPLETE)
         assert len(conference.get_assignment_notes()) == len(conference.get_paper_notes()), \
@@ -243,8 +229,7 @@ class TestEndToEnd(unittest.TestCase):
             DisplayConf(conference, self.silent).show_assignment()
             raise exc
 
-    # @unittest.skip
-    def test8_10papers_7reviewers_3locks (self):
+    def test8_10papers_7reviewers_3locks (self, test_util):
         '''
         Tests 10 papers each requiring 2 reviews.  7 users each capable of giving 4 reviews.  Constraints lock users in 2 papers
         Expects:  Successful production of assignment where locked users are assigned to papers they were locked to.
@@ -258,9 +243,9 @@ class TestEndToEnd(unittest.TestCase):
                       Params.CONSTRAINTS_LOCKS : {0: [0], 1: [1,2]}
                   }
             })
-        self.test_util.set_test_params(params)
-        self.test_util.test_matcher()
-        conference = self.test_util.get_conference()
+        test_util.set_test_params(params)
+        test_util.test_matcher()
+        conference = test_util.get_conference()
         assert conference.get_config_note_status() == Configuration.STATUS_COMPLETE, \
             "Failure: Config status is {} expected {}".format(conference.get_config_note_status(), Configuration.STATUS_COMPLETE)
         assert len(conference.get_assignment_notes()) == len(conference.get_paper_notes()), \
@@ -276,8 +261,7 @@ class TestEndToEnd(unittest.TestCase):
                         "Reviewer {} was locked to paper {} by constraint but not found in assignment".format(reviewer,forum_id)
 
 
-    # @unittest.skip
-    def test9_10papers_7reviewers_10locks (self):
+    def test9_10papers_7reviewers_10locks (self, test_util):
         '''
         Tests 10 papers each requiring 2 reviews.  7 users each capable of giving 4 reviews.  Constraints lock users in all 10 papers
         Expects:  Successful production of assignment where locked users are assigned to papers they were locked to.
@@ -291,9 +275,9 @@ class TestEndToEnd(unittest.TestCase):
                       Params.CONSTRAINTS_LOCKS : {0:[0], 1:[1], 2:[2], 3:[3], 4:[4], 5:[5], 6:[6], 7:[0], 8:[1], 9:[2]}
                   }
             })
-        self.test_util.set_test_params(params)
-        self.test_util.test_matcher()
-        conference = self.test_util.get_conference()
+        test_util.set_test_params(params)
+        test_util.test_matcher()
+        conference = test_util.get_conference()
         assert conference.get_config_note_status() == Configuration.STATUS_COMPLETE, \
             "Failure: Config status is {} expected {}".format(conference.get_config_note_status(), Configuration.STATUS_COMPLETE)
         assert len(conference.get_assignment_notes()) == len(conference.get_paper_notes()), \
@@ -312,8 +296,7 @@ class TestEndToEnd(unittest.TestCase):
             DisplayConf(conference, self.silent).show_assignment()
             raise exc
 
-    # @unittest.skip
-    def test10_10papers_7reviewers_4vetos_8locks (self):
+    def test10_10papers_7reviewers_4vetos_8locks (self, test_util):
         '''
         Tests 10 papers each requiring 2 reviews.  7 users each capable of giving 4 reviews.  Constraints veto users in 4 papers and lock users in 4 papers
         Expects:  Successful production of assignment where locked users are assigned to papers they were locked to and vetoed users are not assigned to papers they were vetoed from.
@@ -327,9 +310,9 @@ class TestEndToEnd(unittest.TestCase):
                       Params.CONSTRAINTS_LOCKS: {0: [4], 2: [4], 4: [1], 5: [1]}
                   }
             })
-        self.test_util.set_test_params(params)
-        self.test_util.test_matcher()
-        conference = self.test_util.get_conference()
+        test_util.set_test_params(params)
+        test_util.test_matcher()
+        conference = test_util.get_conference()
         assert conference.get_config_note_status() == Configuration.STATUS_COMPLETE, \
             "Failure: Config status is {} expected {}".format(conference.get_config_note_status(), Configuration.STATUS_COMPLETE)
         assert len(conference.get_assignment_notes()) == len(conference.get_paper_notes()), \
@@ -351,8 +334,7 @@ class TestEndToEnd(unittest.TestCase):
             DisplayConf(conference, self.silent).show_assignment()
             raise exc
 
-    # @unittest.skip
-    def test11_5papers_4reviewers_conflicts (self):
+    def test11_5papers_4reviewers_conflicts (self, test_util):
         '''
         Tests 5 papers each requiring 2 reviews.  4 users each capable of giving 3 reviews.  6 conflicts are created between papers/reviewers.
         Expects: Not sure if this should fail because of the number of conflicts limiting the supply significantly.
@@ -363,10 +345,10 @@ class TestEndToEnd(unittest.TestCase):
                   Params.REVIEWER_MAX_PAPERS: 3,
                   Params.CONFLICTS_CONFIG : {0: [1,2], 1: [1,2], 3: [3], 4: [3]}
             })
-        self.test_util.set_test_params(params)
-        self.test_util.test_matcher()
+        test_util.set_test_params(params)
+        test_util.test_matcher()
 
-        conference = self.test_util.get_conference()
+        conference = test_util.get_conference()
         assert conference.get_config_note_status() == Configuration.STATUS_COMPLETE, \
             "Failure: Config status is {} expected {}".format(conference.get_config_note_status(), Configuration.STATUS_COMPLETE)
         conflicts = conference.get_conflicts()
@@ -380,8 +362,7 @@ class TestEndToEnd(unittest.TestCase):
             DisplayConf(conference, self.silent).show_assignment()
             raise exc
 
-    # @unittest.skip
-    def test12_5papers_4reviewers_conflicts (self):
+    def test12_5papers_4reviewers_conflicts (self, test_util):
         '''
         Tests 5 papers each requiring 2 reviews.  4 users each capable of giving 3 reviews.  3 conflicts are created between papers/reviewers.
         Expects: A successful match where conflicted users are not assigned to papers that they conflict with.
@@ -392,10 +373,10 @@ class TestEndToEnd(unittest.TestCase):
                          Params.REVIEWER_MAX_PAPERS: 3,
                          Params.CONFLICTS_CONFIG : {0: [0], 1: [1], 3: [3]}
                          })
-        self.test_util.set_test_params(params)
-        self.test_util.test_matcher()
+        test_util.set_test_params(params)
+        test_util.test_matcher()
 
-        conference = self.test_util.get_conference()
+        conference = test_util.get_conference()
         assert conference.get_config_note_status() == Configuration.STATUS_COMPLETE, \
             "Failure: Config status is {} expected {}".format(conference.get_config_note_status(), Configuration.STATUS_COMPLETE)
         conflicts = conference.get_conflicts()
@@ -410,6 +391,6 @@ class TestEndToEnd(unittest.TestCase):
             raise exc
 
 
-
-if __name__ == "__main__":
-    unittest.main()
+#
+# if __name__ == "__main__":
+#     unittest.main()
