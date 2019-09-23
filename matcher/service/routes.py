daf1f2fdd28bd6a3a08e6868c84aa6182f9fe8d7
Michael Spector <michaelspector@gmail.com>
2019-09-23 13:44:50 -0400

    remove outdated reference

--- matcher/service/routes.py
+++ matcher/service/routes.py
@@ -8,7 +8,7 @@ import threading
 import openreview
 
 from matcher import Matcher
-from .openreview_interface import ConfigNoteInterface, InterfaceException
+from .openreview_interface import ConfigNoteInterface
 
 BLUEPRINT = flask.Blueprint('match', __name__)
 
