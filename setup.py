d26a633956c5db69c941d7c2e0aed3059269b1fa
Mohit Uniyal <mohituniyal16apr@gmail.com>
2020-04-10 12:00:22 +0530

    increased version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='openreview-matcher',
-      version='0.1',
+      version='0.2',
       description='OpenReview matching library',
       url='https://github.com/openreview/openreview-matcher',
       author='Michael Spector',
