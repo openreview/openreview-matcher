fe4606759ed94a10ea4dda4827b9f92cfc947cbf
Mohit Uniyal <mohituniyal16apr@gmail.com>
2020-05-01 15:16:35 +0530

    add test docstring for fairflow

--- tests/test_solvers_fairflow.py
+++ tests/test_solvers_fairflow.py
@@ -53,6 +53,13 @@ def test_solvers_fairflow_random():
     assert_arrays(result_B, demands)
 
 def test_solvers_fairflow_custom_demands():
+    '''
+    Tests 3 papers, 4 reviewers.
+    Reviewers review min: 1, max: 2 papers.
+    Papers need [2,1,3] reviews.
+    No constraints.
+    Purpose: Assert that papers demands are matched.
+    '''
     aggregate_score_matrix_A = np.transpose(np.array([
         [0.2, 0.1, 0.4],
         [0.5, 0.2, 0.3],
@@ -73,6 +80,13 @@ def test_solvers_fairflow_custom_demands():
     assert_arrays(result, demands)
 
 def test_solvers_fairflow_custom_supply():
+    '''
+    Tests 3 papers, 4 reviewers.
+    Reviewers review min: 1, max: [2,1,3,1] papers respectively.
+    Each papers needs 2 reviews.
+    No constraints.
+    Purpose: Assert that reviewers are assigned papers correctly based on their supply.
+    '''
     aggregate_score_matrix_A = np.transpose(np.array([
         [0.2, 0.1, 0.4],
         [0.5, 0.2, 0.3],
@@ -93,6 +107,13 @@ def test_solvers_fairflow_custom_supply():
     assert_arrays(result, demands)
 
 def test_solvers_fairflow_custom_demand_and_supply():
+    '''
+    Tests 3 papers, 4 reviewers.
+    Reviewers review min: 0, max: [2,1,3,1] papers.
+    The 3 Papers need 2,1,3 reviews.
+    No constraints.
+    Purpose: Assert that custom demand and supply are matched.
+    '''
     aggregate_score_matrix_A = np.transpose(np.array([
         [0.2, 0.1, 0.4],
         [0.5, 0.2, 0.3],
@@ -113,6 +134,13 @@ def test_solvers_fairflow_custom_demand_and_supply():
     assert_arrays(result, demands)
 
 def test_solvers_fairflow_custom_demands_paper_with_0_demand():
+    '''
+    Tests 3 papers, 4 reviewers.
+    Reviewers review min: 0, max: 2 papers.
+    The 3 Papers need 2,1,0 reviews.
+    No constraints.
+    Purpose: Assert that reviewers demanding 0 papers get 0 assignments.
+    '''
     aggregate_score_matrix_A = np.transpose(np.array([
         [0.2, 0.1, 0.4],
         [0.5, 0.2, 0.3],
@@ -132,9 +160,11 @@ def test_solvers_fairflow_custom_demands_paper_with_0_demand():
     result = [assignments for assignments in np.sum(res_A, axis=1)]
     assert_arrays(result, demands)
 
-def test_solver_no_0_score_assignment():
+def test_solver_fairflow_no_0_score_assignment():
     '''
-    Tests 5 papers, 4 reviewers. Reviewers review min: 1, max: 3 papers. Each paper needs 2 reviews.
+    Tests 5 papers, 4 reviewers.
+    Reviewers review min: 1, max: 3 papers.
+    Each paper needs 2 reviews.
     No constraints.
     Purpose: Assert that an assignment is never made for 0 or less score
     '''
@@ -158,13 +188,12 @@ def test_solver_no_0_score_assignment():
 
     res = solver.solve()
     assert res.shape == (5,4)
-
-    nrows, ncols = res.shape
+    nrows, ncols = res.shape if len(res.shape) == 2 else (0,0)
     for i in range(nrows):
         for j in range(ncols):
             assert not (aggregate_score_matrix[i,j] <= 0 and res[i,j] > 0), "Solution violates the rule for not making less than 0 score assignments at [{},{}]".format(i,j)
 
-def test_solver_impossible_constraints():
+def test_solver_fairflow_impossible_constraints():
     '''
     Test to ensure that the FairFlow solver's 'solved' attribute is correctly set
     when no solution is possible due to constraints.
@@ -192,15 +221,17 @@ def test_solver_impossible_constraints():
 
     assert not solver.solved
 
-def test_solver_respects_constraints():
+def test_solver_fairflow_respects_constraints():
     '''
-    Tests 5 papers, 4 reviewers. Reviewers review min: 1, max: 3 papers. Each paper needs 2 reviews.
+    Tests 5 papers, 4 reviewers.
+    Reviewers review min: 1, max: 3 papers.
+    Each paper needs 2 reviews.
     Constrained such that:
     Reviewer 0: available for all papers
              1: cannot review papers 2,3
              2: cannot review papers 2,3
-             3: cannot review papers 0, 1
-    All scores set to 1 so that any match that does not violate constraints is optimal
+             3: cannot review papers 0,1
+    All scores set to 1 so that any match that does not violate constraints is optimal.
     Purpose:  Honors constraints in its solution
     '''
     aggregate_score_matrix = np.transpose(np.array([
@@ -225,21 +256,23 @@ def test_solver_respects_constraints():
     assert res.shape == (5, 4)
     assert solver.solved
     # make sure result does not violate constraints (i.e. no flow at i,j if there is a -1 constraint at i,j
-    nrows, ncols = res.shape
+    nrows, ncols = res.shape if len(res.shape) == 2 else (0,0)
     for i in range(nrows):
         for j in range(ncols):
             assert not (constraint_matrix[i,j] == -1 and res[i,j] > 0), "Solution violates constraint at [{},{}]".format(i,j)
 
-def test_solver_respect_constraints_2():
+def test_solver_fairflow_respect_constraints_2():
     '''
-    Tests 5 papers, 4 reviewers. Reviewers review min: 1, max: 3 papers. Each paper needs 2 reviews.
+    Tests 5 papers, 4 reviewers.
+    Reviewers review min: 1, max: 3 papers.
+    Each paper needs 2 reviews.
     Constrained such that:
     Reviewer 0: available for all papers
              1: cannot review papers 0,3
              2: cannot review papers 3,4
              3: cannot review papers 1,2
     Scores set such that a lowest-cost solution can be found along all reviewer-paper arcs with cost = -10 and no others.
-    Purpose:  Finds the lowest cost solution in combination with honoring constraints (i.e. ignores lower-cost paths that are constrained to be ommitted)
+    Purpose: Finds the lowest cost solution in combination with honoring constraints (i.e. ignores lower-cost paths that are constrained to be ommitted)
     '''
     aggregate_score_matrix = np.transpose(np.array([
         [-10, 1, 1, -10, -10],
@@ -262,15 +295,17 @@ def test_solver_respect_constraints_2():
     assert res.shape == (5, 4)
     assert solver.solved
     # make sure result does not violate constraints (i.e. no flow at i,j if there is a -1 constraint at i,j
-    nrows, ncols = res.shape
+    nrows, ncols = res.shape if len(res.shape) == 2 else (0,0)
     for i in range(nrows):
         for j in range(ncols):
             assert not (constraint_matrix[i,j] == -1 and res[i,j] > 0), "Solution violates constraint at [{},{}]".format(i,j)
 
-def test_solver_respect_minimums():
+def test_solver_fairflow_respect_minimums():
     '''
-    Tests 6 papers, 6 reviewers.   Reviewers review min: 2, max: 3 papers.   Each paper needs 2 reviews.
-    All scores set to 1 so that any match that does not violate constraints is optimal
+    Tests 6 papers, 6 reviewers.
+    Reviewers review min: 2, max: 3 papers.
+    Each paper needs 2 reviews.
+    All scores set to 1 so that any match that does not violate constraints is optimal.
     Purpose:  Honors minimums == 2 for all reviewers
     '''
     aggregate_score_matrix = np.array([
@@ -299,7 +334,7 @@ def test_solver_respect_minimums():
     assert solver.solved
 
     # make sure every reviewer is reviewing 2 papers
-    nrows, ncols = res.shape
+    nrows, ncols = res.shape if len(res.shape) == 2 else (0,0)
     for rix in range(nrows):
         reviewer_count_reviews = 0
         for pix in range(ncols):
@@ -307,10 +342,13 @@ def test_solver_respect_minimums():
                 reviewer_count_reviews += 1
         assert reviewer_count_reviews == 2
 
-def test_solver_respect_minimums_2():
+def test_solver_fairflow_respect_minimums_2():
     '''
-    Tests 3 papers, 4 reviewers.   Reviewers review min: 1, max: 3 papers.   Each paper needs 3 reviews.
-    Reviewer 4 has very high cost.  Other reviewers have 0 cost.
+    Tests 3 papers, 4 reviewers.
+    Reviewers review min: 1, max: 3 papers.
+    Each paper needs 3 reviews.
+    Reviewer 4 has very high cost.
+    Other reviewers have 0 cost.
     Purpose:  Make sure all reviewers get at least their minimum
     '''
     num_papers = 3
@@ -343,7 +381,7 @@ def test_solver_respect_minimums_2():
     assert res.shape == (3, 4)
     assert solver.solved
     # make sure every reviewer has at least 1 paper
-    nrows, ncols = res.shape
+    nrows, ncols = res.shape if len(res.shape) == 2 else (0,0)
     for rix in range(nrows):
         reviewer_count_reviews = 0
         for pix in range(ncols):
@@ -351,10 +389,13 @@ def test_solver_respect_minimums_2():
                 reviewer_count_reviews += 1
         assert reviewer_count_reviews >= 1
 
-def test_solver_respect_minimums_3():
+def test_solver_fairflow_respect_minimums_3():
     '''
-    Tests 3 papers, 4 reviewers.   Reviewers review min: 2, max: 3 papers.   Each paper needs 3 reviews.
-    Reviewer 4 has very high cost.  Other reviewers have 0 cost.
+    Tests 3 papers, 4 reviewers.
+    Reviewers review min: 2, max: 3 papers.
+    Each paper needs 3 reviews.
+    Reviewer 4 has very high cost.
+    Other reviewers have 0 cost.
     Purpose:  Make sure all reviewers get at least their minimum
     '''
     num_papers = 3
@@ -386,7 +427,7 @@ def test_solver_respect_minimums_3():
     assert res.shape == (3, 4)
     assert solver.solved
     # make sure every reviewer has at least 1 paper
-    nrows, ncols = res.shape
+    nrows, ncols = res.shape if len(res.shape) == 2 else (0,0)
     for rix in range(nrows):
         reviewer_count_reviews = 0
         for pix in range(ncols):
@@ -394,10 +435,13 @@ def test_solver_respect_minimums_3():
                 reviewer_count_reviews += 1
         assert reviewer_count_reviews >= 1
 
-def test_solver_respects_one_minimum():
+def test_solver_fairflow_respects_one_minimum():
     '''
-    Tests 3 papers, 4 reviewers.   Reviewers review min: 1, max: 3 papers.   Each paper needs 3 reviews.
-    Reviewer 4 has very high cost.  Other reviewers have 0 cost.
+    Tests 3 papers, 4 reviewers.
+    Reviewers review min: 1, max: 3 papers.
+    Each paper needs 3 reviews.
+    Reviewer 4 has very high cost.
+    Other reviewers have 0 cost.
     Purpose:  Make sure all reviewers (including reviewer 4) get at least their minimum
     '''
     num_papers = 3
@@ -430,7 +474,7 @@ def test_solver_respects_one_minimum():
     assert res.shape == (3, 4)
     assert solver.solved
     # make sure every reviewer has at least 1 paper
-    nrows, ncols = res.shape
+    nrows, ncols = res.shape if len(res.shape) == 2 else (0,0)
     for rix in range(nrows):
         reviewer_count_reviews = 0
         for pix in range(ncols):
@@ -438,10 +482,13 @@ def test_solver_respects_one_minimum():
                 reviewer_count_reviews += 1
         assert reviewer_count_reviews >= 1
 
-def test_solver_respects_two_minimum():
+def test_solver_fairflow_respects_two_minimum():
     '''
-    Tests 3 papers, 4 reviewers.   Reviewers review min: 2, max: 3 papers.   Each paper needs 3 reviews.
-    Reviewer 4 has very high cost.  Other reviewers have 0 cost.
+    Tests 3 papers, 4 reviewers.
+    Reviewers review min: 2, max: 3 papers.
+    Each paper needs 3 reviews.
+    Reviewer 4 has very high cost.
+    Other reviewers have 0 cost.
     Purpose:  Make sure all reviewers (including reviewer 4) get at least their minimum
     '''
     num_papers = 3
@@ -473,7 +520,7 @@ def test_solver_respects_two_minimum():
     assert res.shape == (3, 4)
     assert solver.solved
     # make sure every reviewer has at least 1 paper
-    nrows, ncols = res.shape
+    nrows, ncols = res.shape if len(res.shape) == 2 else (0,0)
     for rix in range(nrows):
         reviewer_count_reviews = 0
         for pix in range(ncols):
