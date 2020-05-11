d9925dccc77f6dd57ef48f8d11d9c570364870b0
Mohit Uniyal <mohituniyal16apr@gmail.com>
2020-05-11 18:49:28 +0530

    throw correct error msg in minmax

--- tests/test_integration_minmax.py
+++ tests/test_integration_minmax.py
@@ -157,7 +157,7 @@ def test_integration_supply_mismatch_error(openreview_context):
 
     matcher_status = wait_for_status(openreview_client, config_note.id)
     assert matcher_status.content['status'] == 'No Solution'
-    assert matcher_status.content['error_message'] == 'Solver could not find a solution. Adjust your parameters'
+    assert matcher_status.content['error_message'] == 'total demand (100) is out of range when min review supply is (10) and max review supply is (10)'
 
     paper_assignment_edges = openreview_client.get_edges(label='integration-test', invitation=conference.get_paper_assignment_id(conference.get_reviewers_id()))
 
@@ -232,7 +232,7 @@ def test_integration_demand_out_of_supply_range_error(openreview_context):
 
     matcher_status = wait_for_status(openreview_client, config_note.id)
     assert matcher_status.content['status'] == 'No Solution'
-    assert matcher_status.content['error_message'] == 'Solver could not find a solution. Adjust your parameters'
+    assert matcher_status.content['error_message'] == 'total demand (30) is out of range when min review supply is (40) and max review supply is (50)'
 
     paper_assignment_edges = openreview_client.get_edges(label='integration-test', invitation=conference.get_paper_assignment_id(conference.get_reviewers_id()))
 
@@ -631,4 +631,3 @@ def test_routes_already_running_or_complete(openreview_context):
     assert already_complete_response.status_code == 400
     config_note = openreview_client.get_note(config_note.id)
     assert config_note.content['status'] == 'Complete'
-
