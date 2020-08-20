0a06c6ca4abbdff6e6df130b342d9678994b47e5
Melisa Bok <melisabok@gmail.com>
2020-08-20 09:31:32 -0400

    add missing property

--- matcher/service/openreview_interface.py
+++ matcher/service/openreview_interface.py
@@ -17,6 +17,7 @@ class ConfigNoteInterface:
         self.aggregate_score_invitation = self.client.get_invitation(self.config_note.content['aggregate_score_invitation'])
         self.num_alternates = int(self.config_note.content['alternates'])
         self.paper_notes = []
+        self.allow_all_affinities = False
 
         # Lazy variables
         self._reviewers = None
