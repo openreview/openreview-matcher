88b32435dc85706b37510562883027d82c566147
purujitgoyal <purujit@openreview.net>
2022-03-08 13:35:19 -0500

    formatting fix

--- matcher/solvers/fairsequence.py
+++ matcher/solvers/fairsequence.py
@@ -296,13 +296,21 @@ class FairSequence(object):
         demand_required_for_min = np.sum(required_for_min)
         been_restricted = False
 
-        self.logger.debug("#info FairSequence:total paper demand is %d" % remaining_demand)
+        self.logger.debug(
+            "#info FairSequence:total paper demand is %d" % remaining_demand
+        )
         start = time.time()
 
         while remaining_demand:
             if remaining_demand % 1000 == 0:
-                self.logger.debug("#info FairSequence:remaining paper demand is %d" % remaining_demand)
-                self.logger.debug("#info FairSequence:total time elapsed: %s s" % (time.time() - start))
+                self.logger.debug(
+                    "#info FairSequence:remaining paper demand is %d"
+                    % remaining_demand
+                )
+                self.logger.debug(
+                    "#info FairSequence:total time elapsed: %s s"
+                    % (time.time() - start)
+                )
 
             next_paper, next_rev, best_revs_map = self._select_next_paper(
                 matrix_alloc,
@@ -339,8 +347,11 @@ class FairSequence(object):
                 not been_restricted
                 and demand_required_for_min >= remaining_demand
             ):
-                self.logger.debug("#info FairSequence:remaining paper demand (%d) equals total remaining reviewer load LBs ("
-                                  "%d), restricting reviewer supply" % (remaining_demand, demand_required_for_min))
+                self.logger.debug(
+                    "#info FairSequence:remaining paper demand (%d) equals total remaining reviewer load LBs ("
+                    "%d), restricting reviewer supply"
+                    % (remaining_demand, demand_required_for_min)
+                )
                 maximums_copy = np.copy(required_for_min)
 
         return matrix_alloc
@@ -361,7 +372,10 @@ class FairSequence(object):
         if improper_papers:
             proper_papers = np.where(self.demands > 0)[0]
 
-            self.logger.debug("#info FairSequence:Found %d papers with 0 demand, removing them for now" % (self.num_papers - proper_papers.shape[0]))
+            self.logger.debug(
+                "#info FairSequence:Found %d papers with 0 demand, removing them for now"
+                % (self.num_papers - proper_papers.shape[0])
+            )
 
             saved_demands = np.copy(self.demands)
             saved_constraint_matrix = np.copy(self.constraint_matrix)
@@ -375,7 +389,9 @@ class FairSequence(object):
 
         start = time.time()
         self.solution = self.greedy_wef1()
-        self.logger.debug("#info FairSequence:greedy_wef1 took %s s" % (time.time() - start))
+        self.logger.debug(
+            "#info FairSequence:greedy_wef1 took %s s" % (time.time() - start)
+        )
 
         if self.solution is None:
             self.logger.debug(
@@ -386,7 +402,10 @@ class FairSequence(object):
 
             start = time.time()
             self.solution = self.greedy_wef1()
-            self.logger.debug("#info FairSequence:greedy_wef1 (safe_mode off) took %s s" % (time.time() - start))
+            self.logger.debug(
+                "#info FairSequence:greedy_wef1 (safe_mode off) took %s s"
+                % (time.time() - start)
+            )
 
             if self.solution is None:
                 raise SolverException(
@@ -394,7 +413,9 @@ class FairSequence(object):
                 )
 
         if improper_papers:
-            self.logger.debug("#info FairSequence:Adding back papers with 0 demand")
+            self.logger.debug(
+                "#info FairSequence:Adding back papers with 0 demand"
+            )
             self.demands = saved_demands
             self.constraint_matrix = saved_constraint_matrix
             self.affinity_matrix = saved_affinity_matrix
@@ -409,7 +430,9 @@ class FairSequence(object):
             self.solution = soln
 
         self.logger.debug(
-            "#info FairSequence:objective score of solution is {}".format(np.sum(self.affinity_matrix * self.solution))
+            "#info FairSequence:objective score of solution is {}".format(
+                np.sum(self.affinity_matrix * self.solution)
+            )
         )
 
         self.solved = True
