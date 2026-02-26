74353521f9192a198ad4c981126f1f436c6d022b
Melisa Bok <545506+melisabok@users.noreply.github.com>
2026-02-26 14:19:24 -0500

    Increase version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="openreview-matcher",
-    version="2.0.4",
+    version="2.0.5",
     description="OpenReview matching library",
     url="https://github.com/openreview/openreview-matcher",
     author="Michael Spector",
