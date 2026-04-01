71f623adb92790939033c5c6faee074efd50d0b9
Melisa Bok <545506+melisabok@users.noreply.github.com>
2026-04-01 15:26:16 -0400

    fix path

--- tests/conftest.py
+++ tests/conftest.py
@@ -157,7 +157,7 @@ def clean_start_conference_v2(
     venue.submission_stage = openreview.stages.SubmissionStage(
         double_blind=True,
         readers=[
-            openreview.builder.SubmissionStage.Readers.REVIEWERS_ASSIGNED
+            openreview.stages.SubmissionStage.Readers.REVIEWERS_ASSIGNED
         ],
         due_date=now + datetime.timedelta(minutes=10),
         withdrawn_submission_reveal_authors=True,
